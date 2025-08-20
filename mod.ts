e2dc0bc0fe72bd2a25b63c624dee8939a89b29dc
Gnlow <gnlow@pm.me>
2025-08-20 17:43:44 +0900

    crash fixed?

--- mod.ts
+++ mod.ts
@@ -3,21 +3,27 @@ import p from "npm:pyodide@0.28.2/pyodide.js"
 const pyodide = await p.loadPyodide()
 
 await pyodide.loadPackage("micropip")
-const micropip = await pyodide.pyimport("micropip")
-
-await micropip.install(["logica"])
 
 await pyodide.runPythonAsync(`
+    import micropip
+    await micropip.install("logica")
+
     from logica.parser_py import parse
     from logica.compiler import universe
 
-    print(
-        universe.LogicaProgram(
-            parse.ParseFile("""
-                @Engine("sqlite");
-
-                Hello("World");
-            """)["rule"]
-        ).FormattedPredicateSql("Hello")
-    )
+    def compile(program, predicate):
+        return universe.LogicaProgram(
+            parse.ParseFile(program)["rule"]
+        ).FormattedPredicateSql(predicate)
 `)
+
+const compile: (program: string, predicate: string) => string =
+    pyodide.globals.get("compile")
+
+console.log(
+    compile(`
+        @Engine("sqlite");
+
+        Hello("World");
+    `, "Hello")
+)
